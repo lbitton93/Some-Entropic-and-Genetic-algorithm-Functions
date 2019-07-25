6f3c66ebbda331e61398734f1ba2934f507d3710
lbitton93 <36272279+lbitton93@users.noreply.github.com>
2019-07-25 19:13:37 +0100

    Update and rename order_flip.m to genetic_algorithm/order_flip.m

--- genetic_algorithm/order_flip.m
+++ genetic_algorithm/order_flip.m
@@ -2,8 +2,7 @@ function [M] = order_flip(A,O)
 %   The master matrix A represents a template for the permitted links 
 %   (post conditional indep test). Order flip rearranges A in such a
 %   way that it outputs a DAG adjacency matrix displaying permitted 
-%   potential edges for a given order O. flipped Acan be used to reduce
-%   the sample space for DAG structure selection searches.
+%   potential edges for a given order O. flipped 
 
 N = size(O,2);
 M = zeros(N);

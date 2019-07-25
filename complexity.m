789e845c388b9b12d1bc7982e3408f13ed647311
lbitton93 <36272279+lbitton93@users.noreply.github.com>
2019-07-25 15:48:14 +0100

    Update complexity.m

--- complexity.m
+++ complexity.m
@@ -1,4 +1,4 @@
-function [C] = complexity(DAG,states)
+function [C] = complexity(DAG,datanumstates)
 %Complexity of a network for the scoring function
 %   returns the network complexity, arguments are the DAG and the number of
 %   states for each node.
@@ -10,10 +10,10 @@ for i = 1:Size
     A = find(DAG(:,i));
     
     states(A(:));
-    numstates = states(A(:));
+    numstates = datanumstates(A(:));
     numstates;
     qi = prod(numstates);
-    ri = states(i);
+    ri = datanumstates(i);
     
     C = C + qi.*(ri-1);
     

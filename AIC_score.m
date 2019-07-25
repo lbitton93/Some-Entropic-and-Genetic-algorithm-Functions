3889e78e3d964d8027d5a7e85be13dab7011f729
lbitton93 <36272279+lbitton93@users.noreply.github.com>
2019-07-25 15:27:41 +0100

    Update AIC_score.m

--- AIC_score.m
+++ AIC_score.m
@@ -1,4 +1,4 @@
-function [MDL] = AIC_score(M,datavectors,datanumstates)
+function [SCORE] = AIC_score(M,datavectors,datanumstates)
 Entropy = 0;
 for j = 1:size(datavectors,2)
     parents = find(M(:,j))';
@@ -7,6 +7,6 @@ for j = 1:size(datavectors,2)
     
 end
 LL = -size(datavectors,1)*Entropy;
-MDL = LL - 2*complexity(M,datanumstates);
+SCORE = LL - 2*complexity(M,datanumstates);
 
 end

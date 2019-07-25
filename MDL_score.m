0f6688a81d39680d1e05fa88ec2de3f51627baa9
lbitton93 <36272279+lbitton93@users.noreply.github.com>
2019-07-25 13:41:19 +0100

    Update MDL_score.m

--- MDL_score.m
+++ MDL_score.m
@@ -2,9 +2,7 @@ function [MDL] = MDL_score(M,datavectors,datanumstates)
 Entropy = 0;
 for j = 1:size(datavectors,2)
     parents = find(M(:,j))';
- 
-    Entropy = Entropy + entropy_cond3(parents,datavectors,datanumstates,j);
-    
+    Entropy = Entropy + entropy_cond3(parents,datavectors,datanumstates,j);  
 end
 LL = -size(datavectors,1)*Entropy;
 MDL = LL - 0.5*log2(size(datavectors,1))*complexity(M,datanumstates);

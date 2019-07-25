02f0adbb842cbc7614b4f12fc696d9cd05ec2257
lbitton93 <36272279+lbitton93@users.noreply.github.com>
2019-07-25 15:30:42 +0100

    Update MIT_score.m

--- Scores/MIT_score.m
+++ Scores/MIT_score.m
@@ -1,4 +1,5 @@
 function [MIT] = MIT_score(M,datavectors,datanumstates)
+% The total MIT score is composed as the sum of it's local scores
 MIT = 0;
 for j = 1:size(datavectors,2)
     parents = find(M(:,j))'; 
